47353645cfb4f06eeb41918da2d20a0081604441
ofa5er <oueslati.fakher@gmail.com>
2014-07-02 18:56:58 -0400

    ini

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo apt-get install libxrandr-dev libdjvulibre-dev libghc-parsec3-dev libtiff4-dev x11proto-xinerama-dev libpixman-1-dev libopenexr-dev libtiffxx0c2 liblcms1-dev x11proto-randr-dev libxinerama-dev libjasper-dev libilmbase-dev liblqr-1-0-dev libwmf-dev libghc-mtl-dev libxdot4 gir1.2-rsvg-2.0 libgraphviz-dev libpcre3-dev libxcursor-dev
